82ba046ca393d29af1dae27420294e76838f7757
PTang <pattontanges@gmail.com>
2024-04-14 20:40:58 -0700

    Create files

---
+++ nim.py
@@ -0,0 +1,22 @@
+from search import *
+
+# Student(s): Patton Tang
+# Course: CPSC-481
+# Due Date: 2024-04-19
+
+class GameOfNim:
+    def __init__(self):
+        pass
+
+    def funct(self):
+        pass
+
+
+if __name__ == '__main__':
+    '''
+    wgc = WolfGoatCabbage()
+    solution = depth_first_graph_search(wgc).solution()
+    print(solution)
+    solution = breadth_first_graph_search(wgc).solution()
+    print(solution)
+    '''
