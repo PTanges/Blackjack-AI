2cbd87ba8dcb6feebd813df55024a3c4964b4cd0
PTang <pattontanges@gmail.com>
2024-04-15 18:15:10 -0700

    Implementation of game basics for Nim. Remainder are Utility, to_move in Games, and testing terminal state.

--- game_of_nim.py
+++ game_of_nim.py
@@ -19,7 +19,7 @@ funct terminal test(state): {
 funct utility(state, player): {
 	returns +1 for MAX win
 	returns -1 for MIN win
-	- loser det by who picked LAST
+	- loser det by who picked LAST / "Next turn" player wins
 funct to_move(state): {
 	returns player who's turn it is to move
 	- default impl in abstr: Game is enough
@@ -39,35 +39,60 @@ class GameOfNim(Game):
     the form of a list of (x, y) positions, and a board, in the form of
     a list with number of objects in each row."""
 
-    def __init__(self, board=[3,1]):
-        raise NotImplementedError
+    def __init__(self, board=[]):
+        self.board = [int(x) for x in board]
+        self.initial = [int(x) for x in board]
 
     def actions(self, state):
         """Legal moves are at least one object, all from the same row."""
-        return state.moves
+        # pair (a, b) where a is the row and b iters from val-1 as ...[1, val]
+        moves = []
+        for row_index, row_value in enumerate(self.board):
+            for iterator in range(0, row_value):
+                moves.append((row_index, iterator+1))
+            # end for
+        #end for
+        return moves
 
     def result(self, state, move):
-        raise NotImplementedError
+        # Given a state and what action to take... return the resulting state!
+        # state = [(a,b), (c,d)...], and move = (a, b)
+        row_index, sticks_removed = move
+        state[row_index] = state[row_index] - sticks_removed
+        return state
 
     def utility(self, state, player):
         """Return the value to player; 1 for win, -1 for loss, 0 otherwise."""
-        raise NotImplementedError
+        print(player)
+        if self.terminal_test(state):
+            # Todo: Win Check
+            # Check player = winner/loser?
+            return 1
+        return 0
+
 
     def terminal_test(self, state):
         """A state is terminal if there are no objects left"""
-        raise NotImplementedError
+        return self.isBoardEmpty(state)
 
     def display(self, state):
         board = state.board
         print("board: ", board)
 
+    def isBoardEmpty(self, state):
+        _val = True
+        for x in state:
+            if x != 0:
+                _val = False
+        return _val
+
 if __name__ == '__main__':
     nim = GameOfNim(board=[0, 5, 3, 1])  # Creating the game instance
     # nim = GameOfNim(board=[7, 5, 3, 1]) # a much larger tree to search
-    print(nim.initial.board)  # must be [0, 5, 3, 1]
-    print(nim.initial.moves)  # must be [(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (2,
-    1), (2, 2), (2, 3), (3, 1)]
+    print(nim.initial)  # must be [0, 5, 3, 1]
+    print(nim.actions(nim.initial))  # must be [(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (2, 1), (2, 2), (2, 3), (3, 1)]
     print(nim.result(nim.initial, (1, 3)))
+
     utility = nim.play_game(alpha_beta_player, query_player)  # computer moves first
     if (utility < 0):
         print("MIN won the game")
