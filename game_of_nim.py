aa03d748af5725b1e2f59e509cd7d2ed85abaa0d
PTang <pattontanges@gmail.com>
2024-04-18 18:04:53 -0700

    Game will output correct winner & AI will min player's chance of winning properly

--- game_of_nim.py
+++ game_of_nim.py
@@ -5,63 +5,85 @@ import random
 # Course: CPSC-481
 # Due Date: 2024-04-19
 
-class Board:
-    def __init__(self, board=[]):
-        self.board = [int(x) for x in board]
-
 class GameOfNim(Game):
     """Play Game of Nim with first player 'MAX'.
     A state has the player to move, a cached utility, a list of moves in
     the form of a list of (x, y) positions, and a board, in the form of
     a list with number of objects in each row."""
-    _isWin = -1
+    def __init__(self, board=[7, 5, 3, 1]):
+        # self.initial = GameState(to_move='X', utility=0, board={}, moves=moves)
 
-    def __init__(self, board=[]):
-        self.board = Board(board)
-        self.initial = [int(x) for x in board]
-
-    def actions(self, state):
-        """Legal moves are at least one object, all from the same row."""
-        # pair (a, b) where a is the row and b iters from val-1 as ...[1, val]
         moves = []
-        for row_index, row_value in enumerate(state):
+        for row_index, row_value in enumerate(board):
             if row_value == 0: continue
             for iterator in range(0, row_value):
                 moves.append((row_index, iterator+1))
-            # end for
-        #end for
-        return moves
+
+        self.initial = GameState(to_move="Max", utility=0, board=board, moves=moves)
+
+    def actions(self, state):
+        """Legal moves are at least one object, all from the same row."""
+        return state.moves
 
     def result(self, state, move):
-        # Given a state and what action to take... return the resulting state!
-        # state = [(a,b), (c,d)...], and move = (a, b)
+        # Return Gamestate + pre-calculate utility value based on action taken
         if move == None: return state
+        if state.board == None: return state
 
         row_index, sticks_removed = move
-        new_state = state.copy()
-        new_state[row_index] = state[row_index] - sticks_removed
-        return new_state
+        new_board = state.board.copy()
+
+        valid_moves = []
+        new_utility = 0
+        if row_index > (len(state.board)-1):
+            # Players may "cheat" by skipping their turn (invalid action)
+            pass
+        elif sticks_removed > state.board[row_index]:
+            # Players entering a value MORE than the actual sticks will set = max of that row
+            # (invalid action) -modified-> valid action
+            new_board[row_index] = 0
+        else:
+            # Remove sticks
+            new_board[row_index] = state.board[row_index] - sticks_removed
+
+        # Calculate valid actions
+        for row_index, row_value in enumerate(new_board):
+            if row_value == 0: continue
+            for iterator in range(0, row_value):
+                valid_moves.append((row_index, iterator + 1))
+
+        # Swap Players now that action has been taken
+        if state.to_move == "Min":
+            next_player = "Max"
+        else:
+            next_player = "Min"
+
+        # Calculate Utility
+        quantity_of_actions = len(valid_moves)
+        if quantity_of_actions == 0 and next_player == "Max":
+            new_utility = -1
+        elif quantity_of_actions == 0 and next_player == "Min":
+            new_utility = 1
+        else:
+            new_utility = 0
+
+        return GameState(to_move=next_player, utility=new_utility, board=new_board, moves=valid_moves)
+
 
     def utility(self, state, player):
         """Return the value to player; 1 for win, -1 for loss, 0 otherwise."""
-        # Winner determined by if there's all 0's and only ONE 1.
-        _sum = 0
-        for x in player:
-            _sum += x
-        if _sum == 1: self._isWin = 1
-
-        return self._isWin
+        return state.utility
 
     def terminal_test(self, state):
         """A state is terminal if there are no objects left"""
-        return not self.actions(state)
+        return state.utility != 0 or len(state.moves) == 0
 
     def display(self, state):
-        board = state
+        board = state.board
         print("board: ", board)
 
     def to_move(self, state):
-        return state
+        return state.to_move
 
 if __name__ == '__main__':
     nim = GameOfNim(board=[0, 5, 3, 1])  # Creating the game instance
@@ -71,8 +93,8 @@ if __name__ == '__main__':
     print(nim.result(nim.initial, (1, 3)))
     print("\n")
 
-    utility = nim.play_game(alpha_beta_player, query_player)  # computer moves first
-    if (utility < 0):
+    utility = nim.play_game(query_player, alpha_beta_player)  # computer moves first
+    if (utility > 0):
         print("MIN won the game")
     else:
         print("MAX won the game")
